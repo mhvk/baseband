174e66e39a243307d79c09b16ab9eb5dc9e27bd8
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2015-06-03 16:33:38 -0400

    CRC class

--- baseband/tests/test_vlbi_base.py
+++ baseband/tests/test_vlbi_base.py
@@ -1,4 +1,5 @@
-from ..vlbi_base import bcd_encode, bcd_decode
+from ..vlbi_base import bcd_encode, bcd_decode, CRC
+import numpy as np
 from astropy.tests.helper import pytest
 
 
@@ -16,3 +17,22 @@ class TestBCD(object):
     def test_roundtrip(self):
         assert bcd_decode(bcd_encode(15)) == 15
         assert bcd_decode(bcd_encode(8765)) == 8765
+
+
+def test_crc():
+    # Test example from age 4 of
+    # http://www.haystack.mit.edu/tech/vlbi/mark5/docs/230.3.pdf
+    stream = '0000 002D 0330 0000' + 'FFFF FFFF' + '4053 2143 3805 5'
+    crc_expected = '284'
+    crc12 = CRC(0x180f)
+    stream = stream.replace(' ', '').lower()
+    istream = int(stream, base=16)
+    assert '{:037x}'.format(istream) == stream
+    bitstream = np.array([((istream & (1 << bit)) != 0)
+                          for bit in range(37*4-1, -1, -1)], np.bool)
+    crcstream = crc12(bitstream)
+    crc = np.bitwise_or.reduce(crcstream.astype(np.uint32) <<
+                               np.arange(11, -1, -1))
+    assert '{:03x}'.format(crc) == crc_expected
+    fullstream = np.hstack((bitstream, crcstream))
+    assert crc12.check(fullstream)
--- baseband/vlbi_base/utils.py
+++ baseband/vlbi_base/utils.py
@@ -1,31 +1,122 @@
 import warnings
+import numpy as np
 
-__all__ = ['bcd_decode', 'bcd_encode', 'get_frame_rate']
+__all__ = ['bcd_decode', 'bcd_encode', 'get_frame_rate', 'CRC']
 
 
 def bcd_decode(value):
+    try:
+        # Far faster than my routine for scalars
+        return int('{:x}'.format(value))
+    except ValueError:
+        raise ValueError("Invalid BCD encoded value {0}={1}."
+                         .format(value, hex(value)))
+    except TypeError:  # Presumably an array
+        pass
+
     bcd = value
-    result = 0
     factor = 1
-    while bcd > 0:
-        digit = bcd & 0xf
-        if not (0 <= digit <= 9):
+    result = np.zeros_like(value)
+    while np.any(bcd > 0):
+        bcd, digit = divmod(bcd, 0x10)
+        if np.any(digit > 9):
+            if not np.isscalar(digit):
+                value = value[digit > 9][0]
             raise ValueError("Invalid BCD encoded value {0}={1}."
                              .format(value, hex(value)))
         result += digit * factor
         factor *= 10
-        bcd >>= 4
+
     return result
 
 
 def bcd_encode(value):
+    try:
+        # Far faster than my routine for scalars
+        return int('{:d}'.format(value), base=16)
+    except TypeError:  # Presumably an array
+        pass
+
+    result = np.zeros_like(value)
     result = 0
     factor = 1
-    while value > 0:
+    while np.any(value > 0):
         value, digit = divmod(value, 10)
         result += digit*factor
         factor *= 16
     return result
+
+
+class CRC(object):
+    """Cyclic Redundancy Check for a bitstream.
+
+    See https://en.wikipedia.org/wiki/Cyclic_redundancy_check
+
+    Parameters
+    ----------
+    polynomial : int
+        Binary encoded CRC divisor. For instance, that used by Mark 4 headers
+        is 0x180f, or x^12 + x^11 + x^3 + x^2 + x + 1.
+
+    Once initialised, the instance can be used as a function that calculates
+    the CRC, or one can use the `.check` method to check that the CRC at the
+    end of a stream is correct.
+    """
+    def __init__(self, polynomial):
+        self.polynomial = polynomial
+        self.pol_bin = np.array(
+            [int(bit) for bit in '{:b}'.format(polynomial)], dtype=np.int8)
+
+    def __len__(self):
+        return self.pol_bin.size - 1
+
+    def __call__(self, stream):
+        """Calculate CRC for the given stream.
+
+        Parameters
+        ----------
+        stream : array of bool or unsigned int
+            The dimension is treated as the index into the bits.  For a single
+            stream, the array should thus be of type `bool`. Integers represent
+            multiple streams. E.g., for a 64-track Mark 4 header, the stream
+            would be an array of ``np.uint64`` words.
+
+        Returns
+        -------
+        crc : array
+            The crc will have the same dtype as the input stream.
+        """
+        stream = np.hstack((stream, np.zeros((len(self),), stream.dtype)))
+        return self._crc(stream)
+
+    def check(self, stream):
+        """Check that the CRC at the end of the stream is correct.
+
+        Parameters
+        ----------
+        stream : array of bool or unsigned int
+            The dimension is treated as the index into the bits.  For a single
+            stream, the array should thus be of type `bool`. Integers represent
+            multiple streams. E.g., for a 64-track Mark 4 header, the stream
+            would be an array of ``np.uint64`` words.
+
+        Returns
+        -------
+        ok : bool
+             `True` if the calculated CRC is all zero (which should be the
+             case if the CRC at the end of the stream is correct).
+        """
+        return np.all(self._crc(stream.copy()) == 0)
+
+    def _crc(self, stream):
+        """Internal function to calculate the CRC.
+
+        Note that the stream is changed in-place.
+        """
+        pol_bin = (-self.pol_bin).astype(stream.dtype)
+        for i in range(0, len(stream) - len(self)):
+            stream[i:i+pol_bin.size] ^= (pol_bin & stream[i])
+        return stream[-len(self):]
 
 
 def get_frame_rate(fh, header_class):
