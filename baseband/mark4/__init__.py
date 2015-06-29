295485005beb592b74bce6e9d71b061d0ab396e6
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2015-06-29 16:33:38 -0400

    Move Mark 4 to its own directory.

---
+++ baseband/mark4/__init__.py
@@ -0,0 +1,6 @@
+"""Mark 4 VLBI data reader.  Code inspired by Walter Brisken's mark5access.
+See https://github.com/demorest/mark5access.
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from .data import Mark4Data
