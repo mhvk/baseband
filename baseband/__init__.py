d3ae71e84d723c64eb64b51f949ed8e4ece9f8fd
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2015-10-14 21:39:40 -0400

    Remove example import from __init__.py

--- baseband/__init__.py
+++ baseband/__init__.py
@@ -14,4 +14,5 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-    from example_mod import *
+    # from example_mod import *
+    pass
