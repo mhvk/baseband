080ac7b809a7f723e59182e0f9bc95907840ef2d
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2016-06-17 15:33:46 -0400

    Enlarge examples in mark4.__init__, include copy to vdif.

--- baseband/mark4/__init__.py
+++ baseband/mark4/__init__.py
@@ -1,8 +1,67 @@
-"""Mark 4 VLBI data reader.  Code inspired by Walter Brisken's mark5access.
-See https://github.com/demorest/mark5access.
+"""Mark 4 VLBI data reader.
+
+Code inspired by Walter Brisken's mark5access.  See
+https://github.com/demorest/mark5access.
 
 The format itself is described in detail in
 http://www.haystack.mit.edu/tech/vlbi/mark5/docs/230.3.pdf
+
+All files should be opened using :func:`~baseband.mark4.open`.  Opening in
+binary mode provides a normal file reader but extended with methods to read a
+:class:`~baseband.mark4.Mark4Frame`.  For Mark 4 data, the files often do not
+start at a frame boundary, so one has to seek the first frame.  One also has
+to pass in the number of tracks used, and the decade the data were taken, since
+those numbers cannot be inferred from the data themselves.
+
+>>> from baseband import mark4
+>>> sample_file = mark4.__file__.replace('mark4/__init__.py', 'tests/sample.m4')
+>>> fh = mark4.open(sample_file, 'rb')
+>>> fh.find_frame(ntrack=64)
+2696
+>>> frame = fh.read_frame(ntrack=64, decade=2010)
+>>> frame.shape
+(80000, 8)
+
+
+Opening in stream mode wraps the low-level routines such that reading and
+writing is in units of samples.  It also provides access to header information.
+Here, we need to pass in the frame rate so that times for individual samples
+can be calculated (for longer files, this can be calculated from the file).
+
+>>> fh = mark4.open(sample_file, 'rs', ntrack=64, decade=2010,
+...                 frames_per_second=400)
+>>> fh
+<Mark4StreamReader name=... offset=0
+    samples_per_frame=80000, nchan=8, frames_per_second=400, bps=2,
+    (start) time=2014-06-16T07:38:12.47500>
+>>> d = fh.read(6400)
+>>> d.shape
+(6400, 8)
+>>> d[635:645, 0].astype(int)  # first thread
+array([ 0,  0,  0,  0,  0, -1,  1,  3,  1, -1])
+
+Note that the first 640 elements of every frame are set to zero, as those data
+were overwritten by the header.
+
+To set up a file for writing as a stream is possible as well, although
+probably one would prefer to use VDIF file instead.  Here, we set the number of
+samples per frame to 640, so that the invalid parts of the mark4 data are all
+captured in one frame.
+
+>>> from astropy.time import Time
+>>> import astropy.units as u, numpy as np
+>>> from baseband import vdif
+>>> fw = vdif.open('try.vdif', 'ws',
+...                nthread=1, samples_per_frame=640, nchan=8,
+...                framerate=50000*u.Hz, complex_data=False, bps=2, edv=1,
+...                time=Time('2014-06-16T07:38:12.47500'))
+>>> fw.write(d[:640], invalid_data=True)
+>>> fw.write(d[640:])
+>>> fw.close()
+>>> fr = vdif.open('try.vdif', 'rs')
+>>> d2 = fr.read()
+>>> np.all(d == d2)
+True
 """
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
