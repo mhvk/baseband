7c4ab23ed1a1f0d43c06fc12c3108db458cf7b6b
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2020-07-13 14:00:02 -0400

    Use f-strings in file_info

--- baseband/base/file_info.py
+++ baseband/base/file_info.py
@@ -215,37 +215,38 @@ class InfoBase(metaclass=InfoMeta):
             return super().__repr__()
 
         if any('closed' in str(error) for error in self.errors.values()):
-            return 'File closed. Not parsable.'
+            return "File closed. Not parsable."
 
-        result = ''
+        result = []
         for attr in self.attr_names:
             value = getattr(self, attr)
             if isinstance(value, dict):
-                prefix = '\n{}: '.format(attr)
+                result.append('')
+                prefix = f"{attr}: "
                 if attr == 'missing':
                     for msg in sorted(set(self.missing.values())):
                         keys = sorted(set(key for key in self.missing
                                           if self.missing[key] == msg))
-                        result += "{} {}: {}\n".format(prefix,
-                                                       ', '.join(keys), msg)
-                        prefix = ' ' * (len(attr) + 2)
+                        result.append(f"{prefix} {', '.join(keys)}: {msg}")
+                        prefix = ' ' * len(prefix)
                 else:
                     for key, val in value.items():
                         str_val = str(val) or repr(val)
-                        result += "{} {}: {}\n".format(prefix, key, str_val)
-                        prefix = ' ' * (len(attr) + 2)
+                        result.append(f"{prefix} {key}: {str_val}")
+                        prefix = ' ' * len(prefix)
 
             elif value is not None:
                 if isinstance(value, Time):
                     value = Time(value, format='isot', precision=9)
                 elif attr == 'sample_rate':
                     value = value.to(u.MHz)
-                result += '{} = {}\n'.format(attr, value)
+                result.append(f"{attr} = {value}")
 
         if not self:
-            result += '\nNot parsable. Wrong format?\n'
+            result.append('\nNot parsable. Wrong format?')
 
-        return result
+        result.append('')
+        return '\n'.join(result)
 
 
 class FileReaderInfo(InfoBase):
@@ -395,8 +396,8 @@ class FileReaderInfo(InfoBase):
             return int(number_of_frames)
         else:
             self.warnings['number_of_frames'] = (
-                'file contains non-integer number ({}) of frames'
-                .format(number_of_frames))
+                f"file contains non-integer number "
+                f"({number_of_frames}) of frames")
             return None
 
     @info_item(needs='header0')
@@ -532,7 +533,7 @@ class StreamReaderInfo(InfoBase):
                         # If we know this is the right one, raise,
                         # otherwise start bisection.
                         if frame == good + 1:
-                            msg = 'While reading at {}: '.format(fh.tell())
+                            msg = f"While reading at {fh.tell()}: "
                             if isinstance(exc, UserWarning):
                                 self.warnings['continuous'] = msg + str(exc)
                                 return 'fixable gaps'
