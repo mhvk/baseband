52e5fbc5f4f90525634b9c65d4522cb832c2970c
Marten Henric van Kerkwijk <mhvk@astro.utoronto.ca>
2024-07-17 14:11:21 +0200

    Prepare for 4.2 by upping minimum versions

--- baseband/__init__.py
+++ baseband/__init__.py
@@ -8,8 +8,8 @@ from ._astropy_init import *   # noqa
 # ----------------------------------------------------------------------------
 
 # Define minima for the documentation, but do not bother to explicitly check.
-__minimum_python_version__ = '3.8'
-__minimum_astropy_version__ = '5.0'
-__minimum_numpy_version__ = '1.18'
+__minimum_python_version__ = '3.10'
+__minimum_astropy_version__ = '5.1'
+__minimum_numpy_version__ = '1.24'
 
 from .io import file_info, open  # noqa
