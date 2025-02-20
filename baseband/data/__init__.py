9a7d83c119af98c8d38a2d0c519a547f6ecaf04f
Marten H. van Kerkwijk <mhvk@astro.utoronto.ca>
2025-02-20 12:20:06 -0500

    Fix readthedocs and deal with sphinx warning

--- baseband/data/__init__.py
+++ baseband/data/__init__.py
@@ -123,10 +123,12 @@ equivalent to 1024 complete samples (with 64 overlap).
 SAMPLE_VEGAS = _full_path('sample_vegas.raw')
 r"""VEGAS sample, npol=2, obsnchan=32.
 
-Created from a Green Bank Telescope observation of TOI 1898
-dd if=vegas_59332_80137_Jade_1898_1_0001.0000.raw \
-   of=sample_vegas.raw bs=80 count=178
-Initial frame is >50 MB and truncated.  For tests of header functionality only.
+Created from a Green Bank Telescope observation of TOI 1898 with::
+
+   dd if=vegas_59332_80137_Jade_1898_1_0001.0000.raw \
+      of=sample_vegas.raw bs=80 count=178
+
+Initial frame is >50 MB and truncated. For tests of header functionality only.
 """
 
 SAMPLE_GSB_RAWDUMP_HEADER = _full_path('gsb/sample_gsb_rawdump.timestamp')
